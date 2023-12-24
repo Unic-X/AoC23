568f40166097c48e8d73b804a2ab7786ea1a886a
Unic-X <armansinghkshatri69@gmail.com>
2023-12-24 14:49:31 +0530

    Can't figure out part 2

--- src/day22.rs
+++ src/day22.rs
@@ -1,4 +1,4 @@
-use std::fs;
+use std::{fs, time, collections::{HashMap, VecDeque}};
 
 use nom::{IResult, 
     multi::separated_list0, bytes::complete::tag, 
@@ -10,16 +10,17 @@ use nom::{IResult,
 * Day 22
 * Will do other days later
 * Have no clue brain rot has begun 
+* ACcidently deleted once
 */
 
-#[derive(Debug,PartialEq)]
+#[derive(Debug,PartialEq,Hash,Eq)]
 struct Coordinates{
     x:usize,
     y:usize,
     z:usize
 }
 
-#[derive(Debug,PartialEq)]
+#[derive(Debug,PartialEq,Hash,Eq)]
 struct Brick{
     start:Coordinates,
     end:Coordinates
@@ -48,8 +49,8 @@ impl Brick {
             end:new_end
         }
         //Amount that will fall will always be in Z direction 
-        
     }
+
 }
 
 trait Bricks{
@@ -57,9 +58,15 @@ trait Bricks{
     fn intersecting(&self,brick:&Brick)->Vec<&Brick>;
     fn above(&self,brick:&Brick)->Vec<&Brick>;
     fn below(&self,brick:&Brick)->Vec<&Brick>;
+    fn no_other_support(&self,brick_b:&Brick,brick_a:&Brick,fell:&HashMap<&Brick,bool>)->bool;
+
+    //Part 2
+    fn jenga(&self)->Vec<usize>;
 }
 
 impl Bricks for Vec<Brick>{
+    //Detect if  X,Y of any brick lies between the X,Y of other brick 
+
     fn intersecting(&self,brick:&Brick)->Vec<&Brick>{
         self.into_iter()
             .filter(|&b| {
@@ -67,7 +74,7 @@ impl Bricks for Vec<Brick>{
                 b.end.x >= brick.start.x &&
                 b.start.y <= brick.end.y &&
                 b.end.y >= brick.start.y
-            }).filter(|&b| b!=brick).collect::<Vec<_>>()
+            }).collect::<Vec<_>>()
     }
 
     fn find_intersecting(&self,brick:&Brick)->Vec<&Brick>{
@@ -94,8 +101,68 @@ impl Bricks for Vec<Brick>{
                 b.end.z == brick.start.z - 1
             ).collect()
     }
+    fn no_other_support(&self,brick_b:&Brick,brick_a:&Brick,fell:&HashMap<&Brick,bool>)->bool{
+        match fell.get(brick_b) {
+            Some(value) => {
+                if *value==true {
+                    self.below(&brick_a)
+                        .iter()
+                        .filter(|&b| *b!=brick_b)
+                        .filter(|&b| !*fell.get(b).unwrap_or(&false)) 
+                        .collect::<Vec<_>>()
+                        .is_empty()
+                }else {
+                    return false;
+                }
+            }
+            None=>{
+                return false;
+            }
 
+        }
+
+           
+    }
+
+    //Part 2
     
+    fn jenga(&self)->Vec<usize> {
+        let mut all_falls = Vec::new();
+
+        for start in self.into_iter(){
+
+            let mut q = VecDeque::new();
+
+
+            q.push_back(start);
+            let mut fell = HashMap::new();
+
+            while let Some(curr) = q.pop_front() {
+                let above = self.above(curr);
+
+                fell.insert(curr, true); 
+                if above.is_empty(){
+                    break;
+                }
+                
+                for above_brick in above.into_iter(){
+                    if self.no_other_support(curr,above_brick,&fell){
+                        q.push_back(above_brick);        
+                         //If no other support the brick will fall
+                    }
+                    fell.insert(above_brick, false);
+                }
+            }
+          
+        fell.insert(start, false); //Set the starting index to be false
+         all_falls.push(
+                fell.iter().filter(|&(_,v)| *v==true).collect::<Vec<_>>().len()
+            );
+
+        }
+
+        all_falls
+    }
 
 }
 
@@ -120,7 +187,6 @@ fn coordinate_parser(input: &str) -> IResult<&str, Coordinates> {
 fn parse_brick(input:&str)->IResult<&str,Brick>{
     let (input,(start,end)) = separated_pair(coordinate_parser, tag("~"), coordinate_parser)(input)?;
 
-
     Ok((input,Brick{start,end}))
 }
 
@@ -132,16 +198,16 @@ fn parse(input:&str)->IResult<&str,Vec<Brick>>{
 
 
 
-fn part_1(inp:String){
+fn part_1(inp:String)->(usize,Vec<Brick>){
     let (_,mut bricks) = parse(&inp).unwrap();
     
-
+    let now = time::Instant::now();
     //Arrange blocks starting from bottom 
     bricks.sort_by_key(|b| b.start.z.min(b.end.z));
 
     //One can see that the start is always the smaller one that is in every thing x,y,z in most cases in example
     
-  for i in 0..bricks.len() {
+    for i in 0..bricks.len() {
         let brick = &bricks[i];
 
         // Get intersecting bricks based on the x, y coordinates of end and start
@@ -158,40 +224,56 @@ fn part_1(inp:String){
             // Update the brick in the vector
             bricks[i] = brick.fall(amount_to_fall);
         }else{
+            //If there is no other brick below the desired brick
+            //Simply move the brick to the bottom 
             let delta = brick.end.z - brick.start.z;
-
             bricks[i].start.z = 1;
             bricks[i].end.z = bricks[i].start.z + delta;
         }
     }
+
+
+
     let removable = bricks.iter().filter(|&b| {
-                let above = bricks.above(&b);
-                above.is_empty() ||
+        let above = bricks.above(&b);
 
-                !above.iter().any(|&b2|
-                {
-                    bricks.below(&b2)
-                        .iter()
-                        .filter(|&b3| *b3!=b)
-                        .collect::<Vec<_>>()
-                        .is_empty()
-                })
-            }
-        ).collect::<Vec<_>>();
-
-    println!("{:?}",removable.len());
-
-
-
+        above.iter().any(|&b2| // If the brick above has a brick which can be supported by other brick 
+            bricks.below(&b2)
+            .iter()
+            .filter(|&b3| *b3!=b)
+            .collect::<Vec<_>>()
+            .is_empty()
+        )
+        }
+    ).collect::<Vec<_>>();
+    
+    println!("Done in ({:?})",now.elapsed());
+    (removable.len(),bricks)
 
 }
+
+fn part_2(input:String)->usize{
+    let (_,settled_bricks) = part_1(input);
+
+    let all_falls = settled_bricks.jenga();
+
+    let now = time::Instant::now();
+
+    dbg!(&all_falls);
+
+    println!("Done in ({:?})",now.elapsed());
+    println!("Length: {:?}",all_falls.len());
+    all_falls.iter().sum()
+}   
 
 
 fn main() {
     let examp = fs::read_to_string("input/day_22");
     match examp {
         Ok(ok) => {
-            part_1(ok);
+            let p2 = part_2(ok);
+            println!("Part 2 Solution : {p2:?}");
+        
         }
         Err(e) => {
             println!("{}",e);
